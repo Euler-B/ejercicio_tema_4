c61dcda3c1d96c8845426dbc6b3552dfc71e8613
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2023-02-21 20:38:55 -0400

    assigment finalized

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Salida del If
+        int numeroIf = 3;
+        if (numeroIf == 0) {
+            System.out.println("El cero no es ni positivo ni negativo");
+        } else if (numeroIf < 0) {
+            System.out.println("El numero es negativo");
+        } else {
+             System.out.println("El numero es positivo");
+        }
+        //Salida del While
+        System.out.println("");
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+        //Salida del Do While
+        System.out.println("");
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        } while (numeroWhile < 3);
+        //Salida del for
+        System.out.println("");
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+        //Salida del switch
+        System.out.println("");
+        String estacion = "Verano";
+        switch (estacion) {
+            case "Verano":
+            System.out.println("Estamos en Verano");
+            break;
+            case "Invierno":
+            System.out.println("Estamos en Invierno");
+            break;
+            case "Otoño":
+                System.out.println("Estamos en Otoño");
+                break;
+            case "Primavera":
+                System.out.println("Estamos en Primavera");
+        }
+
+
     }
 }
